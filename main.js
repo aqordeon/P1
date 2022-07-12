4dbd4866597e3ceb23037d4337ff8ddc25df6498
AqordeoN <aqordivika@yahoo.co.id>
2022-07-13 06:30:45 +0700

    adding clickcounter and some responsive

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ gsap.utils.toArray('.section').forEach(section => {
         pinSpacing: false
     });
 });
+
+function clicked(clicked_id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.countapi.xyz/hit/aqordeon/${clicked_id}`);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        document.querySelector(`#${clicked_id} .card-counter`).innerHTML = this.response.value;
+    }
+    xhr.send();
+}
